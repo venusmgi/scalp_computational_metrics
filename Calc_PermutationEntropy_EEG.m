33c55dd00d638d47f483e7ff4c924e9d76b9732a
venusmgi <mostaghs@uci.edu>
2025-02-26 17:08:35 -0800

    WIP

--- Calc_PermutationEntropy_EEG.m
+++ Calc_PermutationEntropy_EEG.m
@@ -1,34 +1,60 @@
-function [entBroad,newSize] = calc_PermEntropy_EEG(EEG_filt,electrode,order,delay)
-% Calculates the Permutation Entropy of the electrode chosen
-% Inputs:   EEG_filt:   filtered clean time series;
-%           electrode:  Electrode number of choice (default: CZ electrode)
-%           order:      order of permuation entropy (default 5)
-%           delay:      delay time of permuation entropy (default 1)
-% Outputs:  entBroad:   Permutation entropy 
-%           newSize:    Sample points of EEG_filt
+function [permEnt] = Calc_PermutationEntropy_EEG(data,fs,order,delay,epochLength,startingIndices)
+
+
+% Calculates the Permutation Entropy for all EEG channels and epochs.
+%
+% Inputs:
+%   data       : Filtered clean EEG data (channels x time points).
+%   fs             : Sampling frequency of the EEG data.
+%   order          : Order of permutation entropy (default 5).
+%   delay          : Delay time of permutation entropy (default 1).
+%   epochLength    : Length of each epoch in seconds.
+%   startingIndices: (Optional) Starting indices for epochs.
+%
+% Outputs:
+%   permEnt        : Permutation entropy for each channel and epoch.
+
 %
 % Previously called: "calculate_PermEntropy_Derek.m"
 % Revised from code used in Smith et al. 2021
 % Uses the pec function (Ouyang et al.)
 % Derek Hu (Lopouratory, 2021)
 %
-% V.1.0: Comment code, generalized code to fit generic EEG datsets 
-%        Removed the use of channel labels from function
-%        Allow the choice of electrode to calculate pec of (CZ pref) (DH)
-% V.1.1: Uploaded onto github (DH)
+% V.1.0: Comment code, generalized code to fit generic EEG datasets.
+% V.1.1: Uploaded onto GitHub.
+% V.2.0 : Venus added option to apply entropy calculation for epochs.
+% Removed new_size from the output
 
-% findCz = strcmp(labels,'CZ');
-% if sum(double(findCz))~=1
-%     disp('THIS ONE DIDNT HAVE CZ');
-% else
-%     EEG_Cz = EEG_filt(findCz,:);
-
-    EEG_elec = EEG_filt(electrode,:);
-    [permEnt,~] = pec(EEG_elec,order,delay);
-    entBroad = permEnt;
-    
-% end
-
-    newSize = size(EEG_filt,2);
-
+nChanns = size(data,1);
+if nargin == 6
+    nEpochs = length(startingIndices);
+else
+    nEpochs = floor(size(data,2)./(epochLength*fs));  % number of epochs in data
 end
+
+% Initialize permutation entropy matrix
+permEnt = nan(nEpochs, nChanns);
+
+    for chanId = 1: nChanns
+        for epochId = 1:nEpochs
+            if nargin == 6
+                startInd = startingIndices(epochId);
+                stopInd = startInd + epochLength * fs - 1;
+            else
+                startInd = (epochId - 1) * epochLength * fs + 1;
+                stopInd = epochId * epochLength * fs;
+            end
+
+                        % Ensure indices are within bounds
+            if stopInd > size(data, 2)
+                % stopInd = size(data, 2);
+                break; % Exit the loop, because I don't want info from an epoch size that is shorter than others
+            end
+
+            [permEnt(epochId,chanId),~] = Pec(data(chanId,startInd:stopInd),order,delay);
+        end
+    end
+    
+
+
+return
