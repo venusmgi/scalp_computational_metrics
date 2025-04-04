547d90ec6efb4a2b53305e51ef3f26cf003c6fa7
venusmgi <mostaghs@uci.edu>
2025-04-04 11:24:47 -0700

    Revised by Dr.Lopour; WIP

--- Calc_PermutationEntropy_EEG.m
+++ Calc_PermutationEntropy_EEG.m
@@ -1,18 +1,17 @@
-function [permEnt] = Calc_PermutationEntropy_EEG(data,fs,order,delay,epochLength,startingIndices)
-
-
+function permEnt = Calc_PermutationEntropy_EEG(data,fs,order,delay,epochLength,startingIndices)
 % Calculates the Permutation Entropy for all EEG channels and epochs.
 %
 % Inputs:
-%   data       : Filtered clean EEG data (channels x time points).
-%   fs             : Sampling frequency of the EEG data.
-%   order          : Order of permutation entropy (default 5).
-%   delay          : Delay time of permutation entropy (default 1).
+%   data     : Filtered clean EEG data (channels x time points).
+%   fs       : Sampling frequency of the EEG data.
+%   order    : Order of permutation entropy (default 5).
+%   delay    : Delay time of permutation entropy (default 1).
 %   epochLength    : Length of each epoch in seconds.
 %   startingIndices: (Optional) Starting indices for epochs.
 %
 % Outputs:
-%   permEnt        : Permutation entropy for each channel and epoch.
+%   permEnt  : Permutation entropy for each channel and epoch;
+%               dimension is nEpoch x nChan
 
 %
 % Previously called: "calculate_PermEntropy_Derek.m"
@@ -25,36 +24,29 @@ function [permEnt] = Calc_PermutationEntropy_EEG(data,fs,order,delay,epochLength
 % V.2.0 : Venus added option to apply entropy calculation for epochs.
 % Removed new_size from the output
 
-nChanns = size(data,1);
+% Number of channels and samples in EEG record
+nChan = size(data,1);
+nSamp = size(data,2);
+
+% Determine start and stop indices for each epoch
 if nargin == 6
-    nEpochs = length(startingIndices);
+    [nEpochs, startInd, stopInd] = Calc_Epoch_Indices(nSamp, fs, epochLength, startingIndices);
 else
-    nEpochs = floor(size(data,2)./(epochLength*fs));  % number of epochs in data
+    [nEpochs, startInd, stopInd] = Calc_Epoch_Indices(nSamp, fs, epochLength);
 end
 
 % Initialize permutation entropy matrix
-permEnt = nan(nEpochs, nChanns);
+permEnt = nan(nEpochs, nChan);
 
-    for chanId = 1: nChanns
-        for epochId = 1:nEpochs
-            if nargin == 6
-                startInd = startingIndices(epochId);
-                stopInd = startInd + epochLength * fs - 1;
-            else
-                startInd = (epochId - 1) * epochLength * fs + 1;
-                stopInd = epochId * epochLength * fs;
-            end
+% Loop through each channel and each epoch
+for chanId = 1:nChan
+    for epochId = 1:nEpochs
+        % Extract EEG epoch
+        epochEEG = data(chanId,startInd(epochId):stopInd(epochId));
 
-                        % Ensure indices are within bounds
-            if stopInd > size(data, 2)
-                % stopInd = size(data, 2);
-                break; % Exit the loop, because I don't want info from an epoch size that is shorter than others
-            end
-
-            [permEnt(epochId,chanId),~] = Pec(data(chanId,startInd:stopInd),order,delay);
-        end
+        % Calculate permutation entropy
+        [permEnt(epochId,chanId),~] = Pec(epochEEG,order,delay);
     end
-    
-
+end
 
 return
