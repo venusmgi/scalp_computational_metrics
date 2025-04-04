248cd1060e06ef2feb36c8880b8e259cf5592bf2
venusmgi <mostaghs@uci.edu>
2025-04-04 11:29:09 -0700

    Revised by Dr. Lopour

--- Calc_ShannonEntropy_EEG.m
+++ Calc_ShannonEntropy_EEG.m
@@ -1,17 +1,17 @@
-function entropy = Calc_ShannonEntropy_EEG(data,fs,aveOptimal,epochLength,startingIndices)
+function entropy = Calc_ShannonEntropy_EEG(data,fs,nBins,epochLength,startingIndices)
 
 % Calculates the Shannon entropy value for each channel.
-% 
+%
 % Inputs:
 %   data       : Filtered clean EEG data (channels x time points).
-%   aveOptimal     : Optimal bin count to use (default 390). Use the 
+%   nBins      : Optimal bin count to use (default 390). Use the
 %                    Freedman-Diaconis rule on the signal to calculate:
 %                    nBins = (max(x) - min(x)) / (2 * IQR(x) * length(x)^(-1/3)).
 %   epochLength    : Length of each epoch in seconds.
 %   startingIndices: (Optional) Starting indices for epochs.
 %
 % Output:
-%   entropy        : Shannon entropy value for each channel.
+%   entropy  : Shannon entropy values; dimension is nEpochs x nChan
 %
 % Previously called: "calculateEntropy.m"
 % Code used in Smith et al. 2021
@@ -21,45 +21,34 @@ function entropy = Calc_ShannonEntropy_EEG(data,fs,aveOptimal,epochLength,starti
 % V.1.1: Derek Hu Uploaded onto GitHub.
 % V.2.0: Venus added option to apply entropy calculation for epochs.
 
+% Number of channels and samples in EEG record
+nChan = size(data, 1);
+nSamp = size(data,2);
 
-% Number of channels
-    nChanns = size(data, 1);
+% Determine start and stop indices for each epoch
+if nargin == 5
+    [nEpochs, startInd, stopInd] = Calc_Epoch_Indices(nSamp, fs, epochLength, startingIndices);
+else
+    [nEpochs, startInd, stopInd] = Calc_Epoch_Indices(nSamp, fs, epochLength);
+end
 
-    % Determine number of epochs
-    if nargin == 5
-        nEpochs = length(startingIndices);
-    else
-        nEpochs = floor(size(data,2)./(epochLength*fs));  % number of epochs in data
+% Initialize entropy matrix
+entropy = nan(nEpochs, nChan);
+
+% Calculate entropy for each channel and epoch
+for chanId = 1:nChan
+    for epochInd = 1:nEpochs
+        % Extract EEG epoch
+        epochEEG = data(chanId, startInd(epochInd):stopInd(epochInd));
+
+        % Calculate histogram and probabilities
+        [N, ~] = histcounts(epochEEG, nBins);
+        probs = N ./ sum(N);
+        epochEntropy = -sum(probs .* log2(probs + eps)); % entropy for one epoch and one channel
+
+        % Store entropy value
+        entropy(epochInd, chanId) = epochEntropy;
     end
+end
 
-
-    % Initialize entropy matrix
-    entropy = nan(nEpochs, nChanns);
-
-     % Calculate entropy for each channel and epoch
-    for chan = 1:nChanns
-        for epochInd = 1:nEpochs
-            if nargin == 5
-                startInd = startingIndices(epochInd);
-                stopInd = startInd + epochLength * fs - 1;
-            else
-                startInd = (epochInd - 1) * epochLength * fs + 1;
-                stopInd = epochInd * epochLength * fs;
-            end
-            
-            % Ensure indices are within bounds
-            if stopInd > size(data, 2)
-                % stopInd = size(data, 2);
-                break; % Exit the loop, because I don't want info from an epoch size that is shorter than others
-            end
-            
-            % Calculate histogram and probabilities
-            [N, ~] = histcounts(round(data(chan, startInd:stopInd),4), aveOptimal);
-            probs = N ./ sum(N);
-            entVal = -sum(probs .* log2(probs + eps));
-            
-            % Store entropy value
-            entropy(epochInd, chan) = entVal;
-        end
-    end
 return
