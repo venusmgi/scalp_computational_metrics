0902bccba633c2dc80023865df2f621cc2d8b8f2
venusmgi <mostaghs@uci.edu>
2025-06-02 13:09:58 -0700

    removed the input N and instead just took the length of the binArts input for EEGduration

--- Find_Clean_Indices.m
+++ Find_Clean_Indices.m
@@ -1,4 +1,4 @@
-function indices = Find_Clean_Indices(EEGDuration,fs,binArts,cleanEpochDuration)
+function startIndVec = Find_Clean_Indices(binArts,fs,cleanEpochDuration)
 
 % FIND_CLEAN_INDICES Identifies clean epochs in an EEG recording
 %
@@ -6,29 +6,26 @@ function indices = Find_Clean_Indices(EEGDuration,fs,binArts,cleanEpochDuration)
 % periods free of artifacts.
 %
 % Inputs:
-%   EEGDuration - Duration of the EEG data in data points
-%   fs - Sampling frequency in Hz
 %   binArts - binary array indicating if an index is clean (0) or
 %             artifactual (1)
-%
+%   fs - Sampling frequency in Hz
 %   cleanEpochDuration - Duration of desired clean epochs in seconds
 %
 % Output:
 %   indices - Start indices of clean epochs
 
-
-
 % Initialize variables for clean epoch detection
 cleanEpochSamples = floor(cleanEpochDuration*fs);  % duration of clean epoch in samples
 count = 0;  % counter used to measure duration of clean segment
-indices = [];  % start indices of clean epochs
+startIndVec = [];  % start indices of clean epochs
+EEGDuration = size(binArts,2);
 
 % Iterate through the binary artifact vector to find clean epochs
 for i = 1:EEGDuration
     if binArts(i) == 0  % If it is a clean sample
         count = count + 1;  % add one to counter
         if count == cleanEpochSamples % if we have enough sequential clean samples
-            indices(end+1,1) = i - cleanEpochSamples + 1;  % add starting index to vector
+            startIndVec(end+1,1) = i - cleanEpochSamples + 1;  % add starting index to vector
             count = 0;  % reset duration of clean segment to zero
         end
     else  % Artifact detected
