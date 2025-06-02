05e54ce6b8e6debd3b7d99ffb1ed0187a75666bf
venusmgi <mostaghs@uci.edu>
2025-06-02 13:20:55 -0700

    changed the order of transWidth to avoid confusion

--- Pick_Filter.m
+++ Pick_Filter.m
@@ -4,41 +4,41 @@ function [filterKernel] = Pick_Filter(filterType,fs)
 % Edited by Venus (5.6.2025)
 %
 % This function designs FIR filters using the least-squares method for various EEG frequency
-% bands ('beta', 'alpha', 'theta', 'broadband', 'delta') based on the given sampling frequency.
+% bands ('delta', 'theta', 'alpha', 'beta', 'broadband') based on the given sampling frequency.
 %
 % Inputs:
-%   filterType - String indicating the desired frequency band filter ('beta', 'alpha', 'theta', 'broadband', 'delta').
-%   fs - Sampling frequency of the signal in Hz.
+%   filterType - String indicating the desired frequency band ('delta', 'theta', 'alpha', 'beta', 'broadband')
+%   fs - Sampling frequency of the signal in Hz
 %
 % Outputs:
-%   filterKernel - FIR filter coefficients for the specified band.
+%   filterKernel - FIR filter coefficients for the specified band
 
 % Compute the Nyquist frequency based on the sampling frequency
 Nyq = fs / 2;
 
-% Choose the appropriate filter design based on the specified filter type
+% Choose the appropriate filter design based on the specified frequency band
 switch lower(filterType)
+    % Beta passband filter
     case 'beta'
-        % Validate the sampling frequency for beta filter design
+        % Verify that the sampling frequency is ok for beta filter design
         if fs > 5000
             error('Sampling frequency exceeds 5000 Hz. Design a custom filter for higher frequencies.');
         end
         % Beta band filter parameters
-        lowestFreq = 13; % Lowest frequency in Hz
+        lowestFreq = 13; % lowest frequency in Hz
         secPerCycle = 1/lowestFreq;
-        %filter order should be at least 2 times(secPerCycle*sampleFreq)
+        % Filter order should be at least 2 times(secPerCycle*sampleFreq)
         n = round(13*(secPerCycle*fs)); % Calculate FIR filter order
-        filterBounds =  [13; 30]; % filter bounds in Hz
-        % transision width should be 10-25% of the upper and lower filter bounds
-        transWidth = [0.10; 0.2]; %having different transition width for different frequencies; not related to the filterBounds order
-        f = [0 (1-transWidth(2))*filterBounds(1) filterBounds(1) filterBounds(2) (1+transWidth(1))*filterBounds(2) Nyq]./Nyq;
-        a = [0 0 1 1 0 0]; % Amplitude response
+        filterBounds = [13; 30]; % filter bounds in Hz
+        % Transition width should be 10-25% of the upper and lower filter bounds
+        transWidth = [0.2; 0.1]; % Note that there are different transition widths for each freq. band; not related to the filterBounds order
+        f = [0 (1-transWidth(1))*filterBounds(1) filterBounds(1) filterBounds(2) (1+transWidth(2))*filterBounds(2) Nyq]./Nyq;
+        a = [0 0 1 1 0 0]; % Amplitude response; passband filter
         filterKernel = firls(n,f,a); % Design filter using least-squares method
 
-
-        % Alpha filter
+    % Alpha passband filter
     case 'alpha'
-        % Validate the sampling frequency for alpha filter design
+        % Verify that the sampling frequency is ok for alpha filter design
         if fs > 1000
             error('Sampling frequency exceeds 1000 Hz. Design a custom filter for higher frequencies.');
         end
@@ -46,20 +46,18 @@ switch lower(filterType)
         % Alpha band filter parameters
         lowestFreq = 8; % Lowest frequency in Hz
         secPerCycle = 1/lowestFreq;
-        %2 cycles at the lowest frequency analyzed
+        % Filter order should be at least 2 cycles at the lowest frequency analyzed
         n = round(16*(secPerCycle*fs)); % Calculate FIR filter order
-        filterBounds =  [8; 13]; % filter bounds in Hz
-        % transision width should be 10-25% of the upper and lower filter bounds
+        filterBounds = [8; 13]; % filter bounds in Hz
+        % transition width should be 10-25% of the upper and lower filter bounds
         transWidth = 0.1;
         f = [0 (1-transWidth)*filterBounds(1) filterBounds(1) filterBounds(2) (1+transWidth)*filterBounds(2) Nyq]./Nyq;
         a = [0 0 1 1 0 0]; % Amplitude response
         filterKernel = firls(n,f,a); % Design filter using least-squares method
 
-
-
-        % Theta filter
+    % Theta passband filter
     case 'theta'
-        % Validate the sampling frequency for theta filter design
+        % Verify that the sampling frequency is ok for theta filter design
         if fs > 500
             error('Sampling frequency exceeds 500 Hz. Design a custom filter for higher frequencies.');
         end
@@ -67,52 +65,48 @@ switch lower(filterType)
         % Theta band filter parameters
         lowestFreq = 4;  % Lowest frequency in Hz
         secPerCycle = 1/lowestFreq;
-        %2 cycles at the lowest frequency analyzed
+        % Filter order should be at least 2 cycles at the lowest frequency analyzed
         n = round(8*(secPerCycle*fs)); % Calculate FIR filter order; %cannot go higher than 8 because it will introduce a spike in filter response
         filterBounds = [4;8];
-        transWidth = [0.15; 0.25]; %having different transition width for different frequencies; not related to the filterBounds order
-        f = [0 (1-transWidth(2))*filterBounds(1) filterBounds(1) filterBounds(2) (1+transWidth(1))*filterBounds(2) Nyq]./Nyq;
+        transWidth = [0.25; 0.15]; % Note that there are different transition widths for each freq. band; not related to the filterBounds order
+        f = [0 (1-transWidth(1))*filterBounds(1) filterBounds(1) filterBounds(2) (1+transWidth(2))*filterBounds(2) Nyq]./Nyq;
         a = [0 0 1 1 0 0]; % Amplitude response
         filterKernel = firls(n,f,a); % Design filter using least-squares method
 
-
-        % Broadband filter
+    % Broadband filter
     case 'broadband'
-        % Validate the sampling frequency for broadband filter design
+        % Verify that the sampling frequency is ok for broadband filter design
         if fs > 5000
             error('Sampling frequency exceeds 5000 Hz. Design a custom filter for higher frequencies.');
         end
-        % Broadband filter parameters
+        
+        % Broadband filter parameters, 1-55 Hz
         lowestFreq = 2; % Initial cutoff frequency in Hz
         secPerCycle = 1/lowestFreq;
         n = round(3.5*(secPerCycle*fs));% Calculate FIR filter order
         f = [0 0.25 1 55 58 Nyq]./Nyq;  % Frequency vector
-        a = [0 0 1 1 0 0]; %amplitude response
+        a = [0 0 1 1 0 0]; % Amplitude response
         filterKernel = firls(n,f,a);
 
-
-
     case 'delta'
-        % Validate the sampling frequency for delta filter design
+        % Verify that the sampling frequency is ok for delta filter design
         if fs > 500
             error('Sampling frequency exceeds 500 Hz. Design a custom filter for higher frequencies.');
         end
+        
         % Delta band filter parameters
-
         lowestFreq = 1; % Lowest frequency in Hz
         secPerCycle = 1/lowestFreq;
-        %2 cycles at the lowest frequency analyzed
-        %n must be at least 2 to 5 times ( 1/lowestFreq ) * fs ;
+        % Filter order should be at least 2 cycles at the lowest frequency analyzed
+        % n must be at least 2 to 5 times ( 1/lowestFreq ) * fs ;
         n = round(2*(secPerCycle*fs));
         filterBounds = [1;4];
-        transWidth = 0.1; %having different transition width, to see which one will work better
+        transWidth = 0.1; % Note that there are different transition widths for each freq. band; not related to the filterBounds order
         f = [0 (1-transWidth)*filterBounds(1) filterBounds(1) filterBounds(2) (1+transWidth)*filterBounds(2) Nyq]./Nyq;
         a = [0 0 1 1 0 0];
         filterKernel = fir2(n,f,a);
-
 
     otherwise
         error('Invalid filter type. Choose from: delta, theta, alpha, beta, broadband.');
 end
 end
-
